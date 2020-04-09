b53355e3641d317c46ad54db926777bbadc1385d
Paul Preissner <preissner.paul@gmail.com>
2020-04-09 16:40:00 +0200

    mod CMakeLists and conanfile to pull log4cpp from conan, and locally link/incl stuff -> this needs to be done properly, e.g. by packaging all dependencies as conan libs

--- buildDebugWithConan.bat
+++ buildDebugWithConan.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-conan install .. -u -s build_type=Debug -s compiler.runtime=MTd -s compiler.version=15 -r=conan-rtg
+conan install .. -u -s build_type=Debug -s compiler.runtime=MTd -s compiler.version=15 -r=conan-rtg --build missing
 cmake -G "Visual Studio 15 Win64" .. 
 cmake --build . --target mumlib --config Debug
 cd ..
